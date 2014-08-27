31f0e9e1cc97c625988d519937035d58cfa9a340
Darren McElligott <mcelligottd@quindell.com>
2014-08-27 16:35:04 -0400

    rough initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var lp = require('./lib/init.js');
+
+//lp.add('testOpt1', 5);
+//lp.add('testOpt2', 2);
+//lp.add('testOpt3', 4);
+//lp.add('testOpt4', 1);
+
+//lp.print();
+console.log(lp.trial());
